fd7c5321e50deb44db2962d217b1e6d950bf2cda
unknown <nandinha1280@gmail.com>
2025-04-15 10:27:15 -0300

    finalizando a atividade 4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const valorCompra = parseFloat (prompt("Insira o valor da compra:"));
 
+if (isNaN(valorCompra)) {
+
+    alert("ERRO: este número é inválido!")
+    
+} else {
+
+    if (!(valorCompra >= 100)) {
+
+        alert("Você não ganhou desconto...")
+        
+    } else {
+
+        const valorTotal = valorCompra * 0.9;
+        alert(`Você ganhou 10% de desconto! O valor total da compra a pagar é de R$ ${valorTotal.toFixed(2)}`) 
+
+    }
+}
